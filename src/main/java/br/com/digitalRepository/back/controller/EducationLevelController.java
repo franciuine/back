fcc0aa40345aa8ac779f9f01e9327b09ef1b24f7
Franciuíne <franciuine@gmail.com>
2022-06-16 13:48:54 -0300

    setando conexões mysql

--- src/main/java/br/com/digitalRepository/back/controller/EducationLevelController.java
+++ src/main/java/br/com/digitalRepository/back/controller/EducationLevelController.java
@@ -6,6 +6,8 @@ import br.com.digitalRepository.back.repository.EducationLevelRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +26,11 @@ public class EducationLevelController {
 	@GetMapping("/levels")
 	public List<EducationLevel> findAll() {
 		return educationLevelRepository.findAll();
+	}
+	
+	@PostMapping("/levels")
+	public EducationLevel save(@RequestBody EducationLevel level) {
+		return educationLevelRepository.save(level);
 	}
 
 }
